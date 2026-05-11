05665a2f1d801aa2e43434dce3e83b9d66519371
AnouarMohamed <m.anouar@mundiapolis.ma>
2026-05-12 00:34:50 +0100

    docs(lib): annotate revalidate tags

--- lib/cache/revalidate.ts
+++ lib/cache/revalidate.ts
@@ -1,13 +1,22 @@
 import { revalidateTag } from "next/cache";
 
+/**
+ * Revalidate cached book data.
+ */
 export function revalidateBooksTag() {
   revalidateTag("books");
 }
 
+/**
+ * Revalidate cached recommendations.
+ */
 export function revalidateRecommendationsTag() {
   revalidateTag("recommendations");
 }
 
+/**
+ * Revalidate all catalog-related tags.
+ */
 export function revalidateCatalogTags() {
   revalidateBooksTag();
   revalidateRecommendationsTag();
